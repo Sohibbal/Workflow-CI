8c5f4e74cb1c51ffc752fab753fbf1132fbfcccc
Sohibbal <m.sohibbal5312@student.unri.ac.id>
2025-12-06 01:18:02 +0700

    test53

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,7 +2,7 @@ import pandas as pd
 import mlflow
 import mlflow.sklearn
 import numpy as np
-import dagshub
+# import dagshub
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import (
