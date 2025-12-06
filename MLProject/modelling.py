09ffab5e45202787b44198993e0cba6f37ce54c8
Sohibbal <m.sohibbal5312@student.unri.ac.id>
2025-12-06 10:14:39 +0700

    test82

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,12 +6,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import os
 
+# 👉 FIX: Tetapkan tracking ke folder mlruns di MLProject
+mlflow.set_tracking_uri("file:./mlruns")
+
 def main():
 
-    # Path dataset (sesuaikan dengan file di MLProject folder)
     data_path = os.path.join(os.path.dirname(__file__), "obesity_classification_preprocessing.csv")
 
-    # Load dataset
     df = pd.read_csv(data_path)
 
     X = df.drop(df.columns[-1], axis=1)
@@ -32,21 +33,14 @@ def main():
         rec = recall_score(y_test, preds, average="weighted")
         f1 = f1_score(y_test, preds, average="weighted")
 
-        print("Akurasi:", acc)
-        print("Precision:", prec)
-        print("Recall:", rec)
-        print("F1 Score:", f1)
-
-        # Manual logging
         mlflow.log_metric("accuracy_manual", acc)
         mlflow.log_metric("precision_manual", prec)
         mlflow.log_metric("recall_manual", rec)
         mlflow.log_metric("f1_manual", f1)
 
-        # Save model
         mlflow.sklearn.log_model(model, artifact_path="model")
 
-    print("🎯 Training + Model Logging sukses tersimpan di MLflow!")
+    print("🎯 Training berhasil & model tersimpan oleh MLflow!")
 
 
 if __name__ == "__main__":
